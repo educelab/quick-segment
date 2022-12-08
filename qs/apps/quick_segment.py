0b6dfc981a129976008a143cc4546eaf5d25cc1e
Bruno Athie Teruel <bat223@uky.edu>
2022-12-08 16:56:21 -0500

    Fixed path error when saving

--- qs/apps/quick_segment.py
+++ qs/apps/quick_segment.py
@@ -484,8 +484,8 @@ class MainWindow(QtWidgets.QWidget):
         QtWidgets.QListWidgetItem(uuid, self.segmentation_list).setCheckState(
             Qt.CheckState.Checked)
 
-        write_ordered_vcps(str(seg_dir + "/fromInterpolator"), interpolation)
-        write_metadata(str(seg_dir + "/fromInterpolator"), "fromInterpolator")
+        write_ordered_vcps(str(str(seg_dir) + "/fromInterpolator"), interpolation)
+        write_metadata(str(str(seg_dir) + "/fromInterpolator"), "fromInterpolator")
         print("Points saved out")
 
     def clear_slice(self, vol):
