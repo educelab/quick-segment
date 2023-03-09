a9ebe837cf776061757e828985483354bfc444db
Bruno Athie Teruel <bat223@uky.edu>
2023-03-09 16:29:33 -0500

    Put colormap outside update

--- qs/apps/quick_segment.py
+++ qs/apps/quick_segment.py
@@ -43,6 +43,7 @@ class MainWindow(QtWidgets.QWidget):
         self.window_height = 800
         self.setMinimumSize(self.window_width, self.window_height)
         self.setWindowTitle("Interpolation Segmentation")
+        self.colormap = colors.ListedColormap(cm.get_cmap('viridis', 512)(np.linspace(0.15, 0.85, 256)))
         self.vol = vol
 
         # ------------------------------Window GUI-----------------------------
@@ -383,8 +384,7 @@ class MainWindow(QtWidgets.QWidget):
     def update_slice(self, vol, val):
         self.ax.clear()
         if (self.show_edges_check.isChecked()):
-            colormap = colors.ListedColormap(cm.get_cmap('viridis', 512)(np.linspace(0.15, 0.85, 256)))
-            self.ax.imshow(canny_edge(vol[val], int(self.edge_threshold1.text()), int(self.edge_threshold2.text())), cmap=colormap)
+            self.ax.imshow(canny_edge(vol[val], int(self.edge_threshold1.text()), int(self.edge_threshold2.text())), cmap=self.colormap)
         else:
             self.ax.imshow(vol[val])
 
