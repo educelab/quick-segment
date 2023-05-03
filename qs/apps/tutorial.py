1521301ea963db3241c14678526a1fdd192f9af2
Sydney Chapman <PlutonianPotato@outlook.com>
2023-05-03 10:53:09 -0400

    added seperate pages with videos

--- qs/apps/tutorial.py
+++ qs/apps/tutorial.py
@@ -33,98 +33,178 @@ class TutorialWindow(QtWidgets.QWidget):
         # ------------------------------Window GUI-----------------------------
         # Overall Window layout
         window_layout = QtWidgets.QHBoxLayout()
-        # window_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
-        # self.setLayout(window_layout)
-        
-        # self.window_layout_2 = QtWidgets.QHBoxLayout()
-        # self.window_layout_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
 
-
+        #create tabs widget 
         self.tabs = QtWidgets.QTabWidget()
-        # self.tabs.setTabPosition(QtWidgets.QTabWidget.TabPosition.North)
-        # self.setCentralWidget(self.tabs)
         
-        #----add in gif------
-        self.video_label = QtWidgets.QLabel()
-        click_vid = QtGui.QMovie('qs/basic_tutorial.gif')
-        #around 58:45 x20
-        # click_vid.setScaledSize(QtCore.QSize(580,450))
-        self.video_label.setMovie(click_vid)
-        click_vid.start()
-        
-        #----add in discription----
-        self.description_layout = QtWidgets.QVBoxLayout()
-        self.description_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
-        
+        #----Generation of Info Tabs----     
+        #----Set big font---   
         big_font = QtGui.QFont("San Francisco", 20)
         big_font.setBold(True)
-        #----General Click---->add
+        
+        #--------------------------------General Click------------------------------>add
         self.gc_label = QtWidgets.QLabel("\nAdding Points")
         self.gc_label.setFont(big_font)
-        self.gc_descript_label = QtWidgets.QLabel("Left click on the canvas to add points")
-        #----Long Click Move---->move
+        self.gc_descript_label = QtWidgets.QLabel(" - Left click on the canvas to add points")
+        
+        #----generate gif------
+        self.gcv_label = QtWidgets.QLabel()
+        click_vid = QtGui.QMovie('qs/gifs/click.gif')
+        #around 58:45 x20
+        # click_vid.setScaledSize(QtCore.QSize(580,450))
+        self.gcv_label.setMovie(click_vid)
+        click_vid.start()
+        
+        #add layout for info section
+        self.gc_layout = QtWidgets.QVBoxLayout()
+        self.gc_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.gc_layout.addWidget(self.gc_label)
+        self.gc_layout.addWidget(self.gc_descript_label)
+        
+        #create tab widget
+        self.page_1 =QtWidgets.QWidget()
+        self.p1_layout =  QtWidgets.QVBoxLayout()
+        self.p1_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.p1_layout.addLayout(self.gc_layout)
+        self.p1_layout.addWidget(self.gcv_label) #click vid
+        self.page_1.setLayout(self.p1_layout)
+        
+        #add tab to list of tabs
+        self.p1_index = self.tabs.addTab(self.page_1, "Points")
+        
+        #--------------------------------Long Click Move-------------------------------->move
         self.lcm_label = QtWidgets.QLabel("\nMoving Points")
         self.lcm_label.setFont(big_font)
-        self.lcm_descript_label = QtWidgets.QLabel("Left click on a point then drag and drop it where you want to move it to")
-        #----Canvas Zoom/pan----
+        self.lcm_descript_label = QtWidgets.QLabel(" - Left click on a point then drag and drop it where you want to move it to \n     Note: A point selected to move turns blue")
+        
+        #----generate gif------
+        self.lmcv_label = QtWidgets.QLabel()
+        move_vid = QtGui.QMovie('qs/gifs/move.gif')
+        #around 58:45 x20
+        move_vid.setScaledSize(QtCore.QSize(600,470))
+        self.lmcv_label.setMovie(move_vid)
+        move_vid.start()
+        
+        #add layout for info section
+        self.lmc_layout = QtWidgets.QVBoxLayout()
+        self.lmc_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.lmc_layout.addWidget(self.lcm_label)
+        self.lmc_layout.addWidget(self.lcm_descript_label)
+        
+        #create tab widget
+        self.page_2 =QtWidgets.QWidget()
+        self.p2_layout =  QtWidgets.QVBoxLayout()
+        self.p2_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.p2_layout.addLayout(self.lmc_layout)
+        self.p2_layout.addWidget(self.lmcv_label) #move vid
+        self.page_2.setLayout(self.p2_layout)
+        
+        #add tab to list of tabs
+        self.p2_index = self.tabs.addTab(self.page_2, "Adjust")
+        #---------------------------------Canvas Zoom/pan---------------------------------
         self.c_label = QtWidgets.QLabel("\nCanvas Navigation")
         self.c_label.setFont(big_font)
         self.c_descript_label = QtWidgets.QLabel(" - Scrolling zooms in and out of the canvas\n - Left click hold drag and release allows you to pan")
-        #----Slice Navigation----
-        self.sn_label = QtWidgets.QLabel("\nSlice Navigation")
-        self.sn_label.setFont(big_font)
-        self.sn_descript_label = QtWidgets.QLabel(" - Single arrows move a single slice forward and backwards\n - Double arrows move to the nearest key slice in the given direciton,\n   when no key slice the number of slices indicated by the jump size box \n - The dropdown allows for viewing all of the key slices and navigate to them")
-        #----Shadows----
+        
+        #----generate gif------
+        self.cv_label = QtWidgets.QLabel()
+        cv_vid = QtGui.QMovie('qs/gifs/zoom_pan.gif')
+        #around 58:45 x20
+        cv_vid.setScaledSize(QtCore.QSize(600,470))
+        self.cv_label.setMovie(cv_vid)
+        cv_vid.start()
+        
+        #add layout for info section
+        self.c_layout = QtWidgets.QVBoxLayout()
+        self.c_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.c_layout.addWidget(self.c_label)
+        self.c_layout.addWidget(self.c_descript_label)
+        
+        #create tab widget
+        self.page_3 =QtWidgets.QWidget()
+        self.p3_layout =  QtWidgets.QVBoxLayout()
+        self.p3_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.p3_layout.addLayout(self.c_layout)
+        self.p3_layout.addWidget(self.cv_label) #zoom/pan vid
+        self.page_3.setLayout(self.p3_layout)
+        
+        #add tab to list of tabs
+        self.p3_index = self.tabs.addTab(self.page_3, "Canvas Nav")
+        
+        #---------------------------------Shadows---------------------------------
         self.s_label = QtWidgets.QLabel("\nShadows")
         self.s_label.setFont(big_font)
         self.s_descript_label = QtWidgets.QLabel(" - A shadow is your points from another key slice\n     - Black shadows are for prior key slices \n     - White shadows are for the succeeding key slices")
         
+        #----generate gif------
+        self.sv_label = QtWidgets.QLabel()
+        sv_vid = QtGui.QMovie('qs/gifs/shadows.gif')
+        #around 58:45 x20
+        sv_vid.setScaledSize(QtCore.QSize(600,470))
+        self.sv_label.setMovie(sv_vid)
+        sv_vid.start()
         
-        self.description_layout.addWidget(self.gc_label)
-        self.description_layout.addWidget(self.gc_descript_label)
-        self.description_layout.addWidget(self.lcm_label)
-        self.description_layout.addWidget(self.lcm_descript_label)
-        self.description_layout.addWidget(self.c_label)
-        self.description_layout.addWidget(self.c_descript_label)
+        #add layout for info section
+        self.s_layout = QtWidgets.QVBoxLayout()
+        self.s_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.s_layout.addWidget(self.s_label)
+        self.s_layout.addWidget(self.s_descript_label)
         
-        self.description_layout_2 = QtWidgets.QVBoxLayout()
-        self.description_layout_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        #create tab widget
+        self.page_4 =QtWidgets.QWidget()
+        self.p4_layout =  QtWidgets.QVBoxLayout()
+        self.p4_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.p4_layout.addLayout(self.s_layout)
+        self.p4_layout.addWidget(self.sv_label) #shadows vid
+        self.page_4.setLayout(self.p4_layout)
         
-        self.description_layout_2.addWidget(self.sn_label)
-        self.description_layout_2.addWidget(self.sn_descript_label)
-        self.description_layout_2.addWidget(self.s_label)
-        self.description_layout_2.addWidget(self.s_descript_label)
+        #add tab to list of tabs
+        self.p4_index = self.tabs.addTab(self.page_4, "Shadows")
         
+        #---------------------------------Slice Navigation---------------------------------
+        #need to split this into different pages for shorter gifs to lead
+        
+        self.sn_label = QtWidgets.QLabel("\nSlice Navigation")
+        self.sn_label.setFont(big_font)
+        self.sn_descript_label = QtWidgets.QLabel(" - Single arrows move a single slice forward and backwards\n - Double arrows move to the nearest key slice in the given direciton,\n   when no key slice the number of slices indicated by the jump size box \n - The dropdown allows for viewing all of the key slices and navigate to them")
+        
+        #----generate gif------
+        self.snv_label = QtWidgets.QLabel()
+        snv_vid = QtGui.QMovie('qs/gifs/slice_nav.gif')
+        #around 58:45 x20
+        snv_vid.setScaledSize(QtCore.QSize(680,550))
+        self.snv_label.setMovie(snv_vid)
+        snv_vid.start()
+        
+        #add layout for info section
+        self.sn_layout = QtWidgets.QVBoxLayout()
+        self.sn_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.sn_layout.addWidget(self.sn_label)
+        self.sn_layout.addWidget(self.sn_descript_label)
+        
+        #create tab widget
+        self.page_6 = QtWidgets.QWidget()
+        self.p6_layout =  QtWidgets.QVBoxLayout()
+        self.p6_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        self.p6_layout.addLayout(self.sn_layout)
+        self.p6_layout.addWidget(self.snv_label) #slice nav vid
+        self.page_6.setLayout(self.p6_layout)
+        
+        #add tab to list of tabs
+        self.p6_index = self.tabs.addTab(self.page_6, "Slice Nav")
+        
+        
+        #----button to easly move to next page?-------
         self.next_button = QtWidgets.QPushButton("Next")
         self.next_button.clicked.connect(lambda: self.next_tut_page())
-        self.description_layout.addWidget(self.next_button)
         
-        #  self.window_layout_2.addLayout(self.description_layout_2)
-        
-        self.page_1 =QtWidgets.QWidget()
-        self.p1_layout =  QtWidgets.QHBoxLayout()
-        self.p1_layout.addWidget(self.video_label)
-        self.p1_layout.addLayout(self.description_layout)
-        self.page_1.setLayout(self.p1_layout)
-        
-        self.tabs.addTab(self.page_1, "Points")
-        
-        self.page_2 =QtWidgets.QWidget()
-        self.p2_layout =  QtWidgets.QHBoxLayout()
-        self.p2_layout.addWidget(self.video_label)
-        self.p2_layout.addLayout(self.description_layout_2)
-        self.page_2.setLayout(self.p2_layout)
-        
-        self.tabs.addTab(self.page_2, "Nav")
-        
+        #adding tabs and windows to main window
         window_layout.addWidget(self.tabs)
         self.setLayout(window_layout)
-        
-        # window_layout.addWidget(self.video_label)
-        # window_layout.addLayout(self.description_layout)
+
         
     def next_tut_page(self):
-        self.setLayout(self.window_layout_2)
+        self.tabs.setCurrentIndex(self.p2_index)
         
         
         
